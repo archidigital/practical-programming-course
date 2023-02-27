e8e49dfbbd109defa27310c1945789bae455482f
Cimpoesu Robert <cimpoesu.robert.adrian@gmail.com>
2023-02-27 09:28:33 +0200

    curs 4

--- Mobile/App.tsx
+++ Mobile/App.tsx
@@ -1,161 +1,19 @@
-import React, { useState } from 'react';
-import type { PropsWithChildren } from 'react';
+import React, {useEffect} from 'react';
 import {
   Alert,
   Button,
-  FlatList,
-  Modal,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
+  PermissionsAndroid,
   Text,
   useColorScheme,
   View,
-  Image
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({ children, title }: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-
-type ItemProps = { title: string };
-
-
-type DataType = {
-  id: string;
-  title: string;
-};
-
-type JobListProps = {
-  titlu: string;
-};
-
-function JobsListComponent(props: JobListProps): JSX.Element {
-  const [currentJob, setCurrentJob] = useState();
-  const [modalVisible, setModalVisible] = useState<boolean>(false);
-  const [DATA, setDATA] = useState<DataType[]>([
-    {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-      title: 'First Job',
-    },
-    {
-      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-      title: 'Second Job',
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: 'Third Job',
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: '4 Job',
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: '5 Job',
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: '6 Job',
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: '7 Job',
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: '8 Job',
-    },
-    {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: '9 Job',
-    },
-  ]);
-
-  function deleteItem(title) {
-    setDATA(prev => prev.filter(element => element.title !== title));
-  }
-
-  function showDetails(title) {
-    setCurrentJob(title);
-    setModalVisible(true);
-  }
-
-  function renderJobItem({ title }: ItemProps) {
-    return (
-      <View style={styles.item}>
-        <Text style={styles.title}>{title}</Text>
-        <Button title='Details' onPress={() => showDetails(title)}></Button>
-      </View>
-    );
-  }
-
-  return (
-    <View>
-      <Text style={{ fontSize: 40 }}>{props.titlu}</Text>
-      <FlatList
-        data={DATA}
-        renderItem={({ item }) => renderJobItem(item)}
-        keyExtractor={item => item.id}
-      />
-      <Modal
-        animationType="slide"
-        //transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          setModalVisible(!modalVisible);
-        }}>
-        <Text style={{ fontSize: 35 }}>{currentJob}</Text>
-        <Text>Pickup Address</Text>
-        <Text>Destination Address</Text>
-        <Image
-          source={require('./map.png')}
-        />
-        <Button title='Close' onPress={() => setModalVisible(false)}></Button>
-      </Modal>
-    </View>
-  );
-}
-
-
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import JobsListComponent from './src/components/JobListComponent';
 
 function App(): JSX.Element {
+  const a = 11;
+  const b = 22;
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -166,9 +24,13 @@ function App(): JSX.Element {
     Alert.alert('Test button');
   }
 
+  function sum(a, b) {
+    return a * b;
+  }
+
   return (
-    <View style={{ flex: 1 }}>
-      <Text style={{ fontSize: 50 }}>Home</Text>
+    <View style={{flex: 1}}>
+      <Text style={{fontSize: 50}}>Home</Text>
 
       <JobsListComponent titlu="Jobs List" />
 
@@ -177,40 +39,10 @@ function App(): JSX.Element {
         title="Start shift"
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
-        style={{ position: 'fixed', bottom: 0 }}
+        style={{position: 'fixed', bottom: 0}}
       />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-  },
-  title: {
-    fontSize: 32,
-  },
-});
 
 export default App;
---
+++ Mobile/src/components/JobListComponent.tsx
@@ -0,0 +1,225 @@
+import {PropsWithChildren, useEffect, useState} from 'react';
+import {
+  View,
+  Text,
+  Button,
+  FlatList,
+  Modal,
+  Image,
+  StyleSheet,
+  PermissionsAndroid,
+} from 'react-native';
+import database from '@react-native-firebase/database';
+import MapView, {Marker, Polyline} from 'react-native-maps';
+import Geolocation from 'react-native-geolocation-service';
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+type ItemProps = {title: string};
+
+type LocationType = {
+  latitude: number;
+  longitude: number;
+};
+
+enum StatusTypeEnum {
+  PENDING,
+  IN_PROGRESS,
+  COMPLETED,
+}
+
+type JobType = {
+  pickupAddress: LocationType;
+  destinationAddress: LocationType;
+  status: StatusTypeEnum;
+  title: string;
+};
+
+type JobListProps = {
+  titlu: string;
+};
+
+function JobsListComponent(props: JobListProps): JSX.Element {
+  const [currentJob, setCurrentJob] = useState<JobType>();
+  const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [DATA, setDATA] = useState<JobType[]>([]);
+  const [myLocation, setMyLocation] = useState<{
+    latitude: number;
+    longitude: number;
+  }>();
+
+  useEffect(() => {
+    database()
+      .ref('/jobList')
+      .orderByChild('status')
+      .equalTo(0)
+      .on('value', snapshot => {
+        setDATA(prev => snapshot.val());
+        //console.log('User data: ', snapshot.val());
+      });
+  }, []);
+
+  const requestLocationPermission = async () => {
+    return await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Cool Photo App Camera Permission',
+        message:
+          'Cool Photo App needs access to your camera ' +
+          'so you can take awesome pictures.',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+  };
+
+  useEffect(() => {
+    (async () => {
+      const hasLocationPermission = await requestLocationPermission();
+      if (hasLocationPermission == 'granted') {
+        Geolocation.getCurrentPosition(
+          position => {
+            setMyLocation({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            });
+            // console.log(position);
+          },
+          error => {
+            // See error code charts below.
+            console.log(error.code, error.message);
+          },
+          {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
+        );
+      }
+    })();
+  }, []);
+
+  //function deleteItem(title) {
+  //  setDATA(prev => prev.filter(element => element.title !== title));
+  //}
+
+  function showDetails(job: JobType) {
+    setCurrentJob(job);
+    setModalVisible(true);
+  }
+
+  function renderJobItem(job: JobType) {
+    return (
+      <View style={styles.item}>
+        <Text style={styles.title}>{job.title}</Text>
+        <Button title="Details" onPress={() => showDetails(job)}></Button>
+      </View>
+    );
+  }
+
+  console.log(currentJob);
+
+  return (
+    <View>
+      <Text style={{fontSize: 40}}>{props.titlu}</Text>
+      <FlatList
+        data={DATA}
+        renderItem={({item}) => renderJobItem(item)}
+        //keyExtractor={item => item.id}
+      />
+      <Modal
+        animationType="slide"
+        //transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}>
+        <Text style={{fontSize: 35}}>{currentJob?.title}</Text>
+        <Text>Pickup Address</Text>
+        <Text>Destination Address</Text>
+        {currentJob != undefined ? (
+          <MapView
+            zoomControlEnabled={true}
+            style={{width: '100%', height: '80%'}}
+            initialRegion={{
+              latitude: 44,
+              longitude: 26,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}>
+            <Marker
+              title="Pickup Address"
+              coordinate={{
+                latitude: currentJob.pickupAddress.latitude,
+                longitude: currentJob.pickupAddress.longitude,
+              }}
+            />
+
+            <Marker
+              title="Destination Address"
+              coordinate={{
+                latitude: currentJob.destinationAddress.latitude,
+                longitude: currentJob.destinationAddress.longitude,
+              }}
+            />
+            <Polyline
+              coordinates={[
+                {
+                  latitude: currentJob.pickupAddress.latitude,
+                  longitude: currentJob.pickupAddress.longitude,
+                },
+                {
+                  latitude: currentJob.destinationAddress.latitude,
+                  longitude: currentJob.destinationAddress.longitude,
+                },
+              ]}
+              strokeColor="#000" // fallback for when `strokeColors` is not supported by the map-provider
+              strokeWidth={6}
+            />
+            {myLocation != undefined ? (
+              <Marker
+                title="My location"
+                coordinate={{
+                  latitude: myLocation?.latitude,
+                  longitude: myLocation?.longitude,
+                }}
+              />
+            ) : null}
+          </MapView>
+        ) : null}
+        <Button title="Close" onPress={() => setModalVisible(false)}></Button>
+      </Modal>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+
+export default JobsListComponent;
